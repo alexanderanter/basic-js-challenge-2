0f7801be55a78ce9e3c94466745e63641fc24cb3
Alexander Anter <alexanderanter@gmail.com>
2016-03-12 21:13:50 +0100

    small progress on the html to jade challenge

--- src/Node.js
+++ src/Node.js
@@ -1,11 +1,28 @@
 "use strict";
 
 function Node(name, level, text) {
-
+  this.name = name;
+  this.level = level;
+  this.text = text;
 }
 
 Node.prototype.toString = function(){
+  var result = "";
+  for(var i = 0; i < this.level; i++) {
+    result += "\t";
+  }
+  if(this.name){
+    result += this.name;
+  }
+  if(this.text){
+      result += " " + this.text;
+  }
 
+
+
+
+
+  return result;
 };
 // Exports
 module.exports = Node;
