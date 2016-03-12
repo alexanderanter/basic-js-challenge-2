e9147ba964fb8f186552806822299564cfaef4dd
Alexander Anter <alexanderanter@gmail.com>
2016-03-12 22:42:28 +0100

    cleaning up the replaceString function

--- src/Node.js
+++ src/Node.js
@@ -5,33 +5,36 @@ function Node(name, level, text) {
   this.level = level;
   this.text = text;
 }
-function replaceString(stringReplace, toReplace, newString) {
-  var re = new RegExp(toReplace, "gi");
-  var replaceWith = newString;
-  var replacedString = stringReplace.replace(re, replaceWith);
-  return replacedString;
+
+function replaceString(stringReplace, oldString, newString) {
+  var re = new RegExp(oldString, "gi");
+  return stringReplace.replace(re, newString);
 }
+
 Node.prototype.toString = function(){
   var result = "";
   var tabString = "";
   var textString = "";
-  var re;
+
   for(var i = 0; i < this.level; i++) {
     tabString += "\t";
   }
+
   result += tabString;
-  if(this.name){
+  if(this.name) {
     result += this.name;
     console.log(this.name);
   }
-  if(this.text){
+
+  if(this.text) {
     textString += " " + this.text;
   }
-
+//first repleace all \n with correct tab value
   textString = replaceString(textString, "\n", "\n" + tabString + "\t");
+// then check for any line breaks that leads with a whitespace and replace that whitespace with a full stop.
   textString = replaceString(textString, " \n", ".\n");
 
-  if(textString != "") {
+  if(textString !== "") {
       result += textString;
   }
   // return textString;
